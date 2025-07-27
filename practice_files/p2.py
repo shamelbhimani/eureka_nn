2c90027dc2cd6769a1577c475f111dbe5c09a3b9
Shamel Bhimani <shamelbhimani@outlook.com>
2025-07-27 14:57:36 -0400

    Added function using numpy

--- practice_files/p2.py
+++ practice_files/p2.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def calculate_neuron(i: list[int | float], weight: list[int | float],
               bias: int | float) -> float:
     total = 0
@@ -32,6 +34,32 @@ def zip_calculate(input_layer: list[int | float],
     total += neuron_bias
     return round(total, 2)
 
+# We can make this process even simpler, and save our effort by using numpy's
+# dot product function, 'np.dot(x, y)'. The dot product function allows us to
+# sum the products of two vectors at a given location i such that i = 1, 2,
+# ..., n. For example the dot product of a vector a = [1, 2] and a vector b =
+# [3, 4] is (a_0 * b_0) + (a_1 * b_1).
+
+# A matrix is simply an array of two or more vectors. The way the np.dot
+# function works in terms of multiplying matrices to vectors is that the
+# first set of values passed into the function must be a matrix. The reason
+# for this is that the function is designed to unpack matrices into arrays,
+# and then index those arrays when performing the operation so that it goes
+# through all the indexes/weights provided in the matrix without throwing a
+# shape error. Note, however, the shape of the array in the matrix must match
+# the shape of the array passed as the 2nd parameter.
+
+
+# Notice how the length of the matrix, i.e., the number of weights equals the
+# number of biases. This is because the function indexes through the matrix,
+# and matches the index of the bias upon calculation.
+def dot_calculate_layer(input_layer: list[int | float],
+                        matrix_of_weights: list[list[int | float]],
+                        list_of_biases: list[int | float]) -> list[float]:
+    output_layer = np.dot(matrix_of_weights, input_layer) + list_of_biases
+    return output_layer # Notice how we've eliminated a whole calculation
+    # function, and reduced the function to an effective 1 line.
+
 try:
     inputs = [1, 2, 3, 2.5]
     weights = [[0.2, 0.8, -0.5, 1.0],
@@ -40,5 +68,6 @@ try:
     biases = [2, 3, 0.5]
     print(calculate_layer(inputs, weights, biases))
     print(zip_calculate_layer(inputs, weights, biases))
+    print(dot_calculate_layer(inputs, weights, biases))
 except Exception as e:
     print(e)
