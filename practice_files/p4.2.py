95b2b3da72f3c6d6fae1c6f689614a18b64715db
Shamel Bhimani <shamelbhimani@outlook.com>
2025-07-28 15:44:51 -0400

    Created class of LayerDense, added initialization and forward functions

---
+++ practice_files/p4.2.py
@@ -0,0 +1,30 @@
+# Classes and Objects with Neural Network Layers
+
+import numpy as np
+
+X = [
+    [1, 2, 3, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]
+]
+
+
+class LayerDense:
+    def __init__(self, n_inputs, n_neurons):
+        self.output = None
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+try:
+    layer_1 = LayerDense(4, 5)
+    layer_2 = LayerDense(5, 2)
+    layer_1.forward(X)
+    print("Layer 1 output:", layer_1.output)
+    layer_2.forward(layer_1.output)
+    print("Layer 2 output:", layer_2.output)
+except Exception as e:
+    print(e)
