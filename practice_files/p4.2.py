126f1807138d2ae996f5e3899c1e8e9c830580d1
Shamel Bhimani <shamelbhimani@outlook.com>
2025-07-28 15:52:34 -0400

    Updated typehints

--- practice_files/p4.2.py
+++ practice_files/p4.2.py
@@ -10,12 +10,12 @@ X = [
 
 
 class LayerDense:
-    def __init__(self, n_inputs, n_neurons):
+    def __init__(self, n_inputs: int, n_neurons: int) -> None:
         self.output = None
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
-    def forward(self, inputs):
+    def forward(self, inputs: object | list[list[int | float]]) -> None:
         self.output = np.dot(inputs, self.weights) + self.biases
 
 
