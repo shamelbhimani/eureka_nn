8ae1cc7b743cdda471f71e4331d01a147203c9a8
Shamel Bhimani <shamelbhimani@outlook.com>
2025-08-01 15:04:04 -0400

    FIXED: typehints, PEP8 file formatting

--- practice_files/p5.py
+++ practice_files/p5.py
@@ -10,14 +10,13 @@ class LayerDense:
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
-    def forward(self, inputs: np.ndarray) -> None:
+    def forward(self, inputs: np.ndarray | list[list[int | float]]) -> None:
         self.output = np.dot(inputs, self.weights) + self.biases
 
 
 class ActivationReLU:
     def __init__(self) -> None:
         self.output = None
-
 
     def forward(self, inputs: np.ndarray | list[list[int | float]]) -> None:
         self.output = np.maximum(0, inputs)
