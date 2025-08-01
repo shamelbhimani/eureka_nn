4afa49a3f720b294b6d135c35ad3c69fb09f2dab
Shamel Bhimani <shamelbhimani@outlook.com>
2025-08-01 14:54:35 -0400

    ADDED: LayerDense class, initialized and added forward function

--- practice_files/p5.py
+++ practice_files/p5.py
@@ -3,3 +3,12 @@ import nnfs
 from nnfs.datasets import spiral_data
 
 nnfs.init()
+
+class LayerDense:
+    def __init__(self, n_inputs: int, n_neurons: int) -> None:
+        self.output = None
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward (self, inputs: np.ndarray) -> None:
+        self.output = np.dot(inputs - self.weights) + self.biases
